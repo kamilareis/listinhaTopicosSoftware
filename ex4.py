e580dc4907a0c7be201a4d370e8d93de111d5c2a
kamilareis <kamilareis2@gmail.com>
2023-09-03 02:52:33 -0300

    Exercicio 4 da Lista de Exercicios

---
+++ ex4.py
@@ -0,0 +1,16 @@
+'''“Banco de dados” de dicionários.'''
+
+'''Programa interativo de Banco de dados que armazena os dados digitados pelo usuario,
+o mesmo define os campos obrigatórios e possiveis campos extras, o programa permite
+filtrar por algum campo especifico ou imprimir todos os usuários cadastrados.'''
+
+
+def menu():
+    print("-----------Menu-------------")
+    print("| 1 - Cadastrar usuário    |")
+    print("| 2 - Imprimir usuários    |")
+    print("| 0 - Encerrar             |")
+    print("----------------------------")
+    opcao = input("Escolha uma opção: ")
+
+menu()
